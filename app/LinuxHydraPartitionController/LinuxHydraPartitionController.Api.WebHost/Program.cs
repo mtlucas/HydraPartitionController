86bd39d6907acb8c97892d5c83b2f6855eb0ab99
wesley.nevans <wesley.nevans@wolterskluwer.com>
2020-12-18 14:44:45 -0600

    DEVOPS-1931 - Start by copying existing project.

--- app/LinuxHydraPartitionController/LinuxHydraPartitionController.Api.WebHost/Program.cs
+++ app/LinuxHydraPartitionController/LinuxHydraPartitionController.Api.WebHost/Program.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Linux_Hydra_Partition_Controller
+namespace LinuxHydraPartitionController.Api.WebHost
 {
     public class Program
     {
@@ -18,6 +18,9 @@ namespace Linux_Hydra_Partition_Controller
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
