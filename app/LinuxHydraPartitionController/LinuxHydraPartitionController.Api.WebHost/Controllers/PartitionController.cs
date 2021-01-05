2c21dab75a7b81f62f336fb530a648b7153b7340
wesley.nevans <wesley.nevans@wolterskluwer.com>
2021-01-05 08:50:24 -0600

    DEVOPS-1931 - Near limit of what can be done without bash scripts or service.

--- app/LinuxHydraPartitionController/LinuxHydraPartitionController.Api.WebHost/Controllers/PartitionController.cs
+++ app/LinuxHydraPartitionController/LinuxHydraPartitionController.Api.WebHost/Controllers/PartitionController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Http;
 
 namespace LinuxHydraPartitionController.Api.WebHost.Controllers
 {
@@ -12,15 +16,22 @@ namespace LinuxHydraPartitionController.Api.WebHost.Controllers
         private readonly ILogger<Partition> _logger;
         private readonly IEnumerable<Partition> _partitions;
         
-        public PartitionController(ILogger<Partition> logger)
+        public PartitionController(ILogger<Partition> logger, IConfiguration configuration)
         {
-            _logger = logger;
-            //Get partitions
-            _partitions = Enumerable.Range(1, 5).Select(index => new Partition(1)).ToArray();
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            if (configuration is null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+            var partitionIds = configuration.GetValue<List<int>>("PartitionIds");
+            _partitions = partitionIds
+                .Select(index => new Partition(index))
+                .ToArray();
         }
 
         [HttpGet]
-        [Produces("application/json")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IEnumerable<Partition> Get()
         {
             return _partitions;
@@ -28,6 +39,8 @@ namespace LinuxHydraPartitionController.Api.WebHost.Controllers
 
         [HttpPost]
         [Route("[controller]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public void Restart(int id)
         {
             _partitions.ToArray()
--- app/LinuxHydraPartitionController/LinuxHydraPartitionController.Api.WebHost/Partition.cs
+++ app/LinuxHydraPartitionController/LinuxHydraPartitionController.Api.WebHost/Partition.cs
@@ -1,13 +1,23 @@
-using System;
+using System.Diagnostics;
 
 namespace LinuxHydraPartitionController.Api.WebHost
 {
     public class Partition
     {
         private readonly int _id;
+        private readonly ProcessStartInfo _processStartInfo;
+        
         internal Partition(int id)
         {
             _id = id;
+            _processStartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash", 
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                Arguments = $"-c 'sudo /usr/bin/systemctl restart gos_{id}'"
+            }; 
         }
 
         public bool IdMatches(int id)
@@ -17,7 +27,9 @@ namespace LinuxHydraPartitionController.Api.WebHost
 
         public void Restart()
         {
-            
+            var proc = new Process { StartInfo = _processStartInfo };
+            proc.Start();
+            var error = proc.StandardError;
         }
     }
 }
