3569e2a055d40dcf3de59be23ef39156a32022b1
Mike.Lucas <mike.lucas@wolterskluwer.com>
2022-04-03 13:52:31 -0500

    DEVOPS-2461 - Change variable names without underscore

--- app/LinuxHydraPartitionController.Api.WebHost/Metrics.cs
+++ app/LinuxHydraPartitionController.Api.WebHost/Metrics.cs
@@ -24,27 +24,26 @@ namespace LinuxHydraPartitionController.Api.WebHost
         public MachineMetrics GetMetrics()
         {
             var manageProcess = new ManageProcess(_logger);
-            MachineMetrics _metrics = new MachineMetrics();
-            ProcessStartInfo _metricsProcessStartInfo = manageProcess.BuildProcessStartInfo("/usr/bin/nproc;/usr/bin/cat /proc/loadavg|/usr/bin/awk '{print $1\\\"\\n\\\"$2\\\"\\n\\\"$3}'");
-            var cpuExec = manageProcess.Execute(_metricsProcessStartInfo);
-            var cpuLines = cpuExec.Split("\n");
+            MachineMetrics machineMetrics = new MachineMetrics();
+            ProcessStartInfo machineMetricsProcessStartInfo = manageProcess.BuildProcessStartInfo("/usr/bin/nproc;/usr/bin/cat /proc/loadavg|/usr/bin/awk '{print $1\\\"\\n\\\"$2\\\"\\n\\\"$3}'");
+            var cpuLines = manageProcess.Execute(machineMetricsProcessStartInfo).Split("\n");
             _logger.LogInformation($"CPU Line 0 and 3: {cpuLines[0]} {cpuLines[3]}");
             _logger.LogInformation("CPU Line 0 Int: ", Convert.ToInt32(cpuLines[0].ToString()));
-            _metrics.CPU.Cores = Convert.ToInt32(cpuLines[0].ToString());
-            _metrics.CPU.Load1min = Convert.ToSingle(cpuLines[1].ToString());
-            _metrics.CPU.Load5min = Convert.ToSingle(cpuLines[2].ToString());
-            _metrics.CPU.Load15min = Convert.ToSingle(cpuLines[3].ToString());
-            _metricsProcessStartInfo = manageProcess.BuildProcessStartInfo("/usr/bin/free -m|/usr/bin/head -2|/usr/bin/tail -n +2|/usr/bin/awk '{print $2\\\"\\n\\\"$3\\\"\\n\\\"$4\\\"\\n\\\"$6\\\"\\n\\\"$7}'");
-            var memlines = manageProcess.Execute(_metricsProcessStartInfo).Split("\n");
-            _metrics.MemoryInMB.Total = Convert.ToInt32(memlines[0].ToString());
-            _metrics.MemoryInMB.Used = Convert.ToInt32(memlines[1].ToString());
-            _metrics.MemoryInMB.Free = Convert.ToInt32(memlines[2].ToString());
-            _metrics.MemoryInMB.Buffers = Convert.ToInt32(memlines[3].ToString());
-            _metrics.MemoryInMB.Available = Convert.ToInt32(memlines[4].ToString());
-            _metricsProcessStartInfo = manageProcess.BuildProcessStartInfo("/usr/bin/cat /proc/uptime|/usr/bin/awk '{print int($1)}'");
-            var uptimeLines = manageProcess.Execute(_metricsProcessStartInfo).Split("\n");
-            _metrics.UptimeInSeconds.Uptime = Convert.ToInt32(uptimeLines[0].ToString());
-            return _metrics;
+            machineMetrics.CPU.Cores = Convert.ToInt32(cpuLines[0].ToString());
+            machineMetrics.CPU.Load1min = Convert.ToSingle(cpuLines[1].ToString());
+            machineMetrics.CPU.Load5min = Convert.ToSingle(cpuLines[2].ToString());
+            machineMetrics.CPU.Load15min = Convert.ToSingle(cpuLines[3].ToString());
+            machineMetricsProcessStartInfo = manageProcess.BuildProcessStartInfo("/usr/bin/free -m|/usr/bin/head -2|/usr/bin/tail -n +2|/usr/bin/awk '{print $2\\\"\\n\\\"$3\\\"\\n\\\"$4\\\"\\n\\\"$6\\\"\\n\\\"$7}'");
+            var memlines = manageProcess.Execute(machineMetricsProcessStartInfo).Split("\n");
+            machineMetrics.MemoryInMB.Total = Convert.ToInt32(memlines[0].ToString());
+            machineMetrics.MemoryInMB.Used = Convert.ToInt32(memlines[1].ToString());
+            machineMetrics.MemoryInMB.Free = Convert.ToInt32(memlines[2].ToString());
+            machineMetrics.MemoryInMB.Buffers = Convert.ToInt32(memlines[3].ToString());
+            machineMetrics.MemoryInMB.Available = Convert.ToInt32(memlines[4].ToString());
+            machineMetricsProcessStartInfo = manageProcess.BuildProcessStartInfo("/usr/bin/cat /proc/uptime|/usr/bin/awk '{print int($1)}'");
+            var uptimeLines = manageProcess.Execute(machineMetricsProcessStartInfo).Split("\n");
+            machineMetrics.UptimeInSeconds.Uptime = Convert.ToInt32(uptimeLines[0].ToString());
+            return machineMetrics;
         }
     }
 }
