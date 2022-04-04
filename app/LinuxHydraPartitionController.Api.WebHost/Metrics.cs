9cb8c87d4c3abd5c44f1aa4ec8c4175e83e1b3d6
Mike.Lucas <mike.lucas@wolterskluwer.com>
2022-04-04 10:45:14 -0500

    DEVOPS-2461 - More attempts at Nullable reference fix

--- app/LinuxHydraPartitionController.Api.WebHost/Metrics.cs
+++ app/LinuxHydraPartitionController.Api.WebHost/Metrics.cs
@@ -26,23 +26,29 @@ namespace LinuxHydraPartitionController.Api.WebHost
         public MachineMetrics GetMetrics()
         {
             var manageProcess = new ManageProcess(_logger);
-            var machineMetrics = new MachineMetrics();
+            CPU cPU = new CPU();
+            MemoryInMB memoryInMB = new MemoryInMB();
+            UptimeInSeconds uptimeInSeconds = new UptimeInSeconds();
+            MachineMetrics machineMetrics = new MachineMetrics();
             ProcessStartInfo machineMetricsProcessStartInfo = manageProcess.BuildProcessStartInfo("/usr/bin/nproc;/usr/bin/cat /proc/loadavg|/usr/bin/awk '{print $1\\\"\\n\\\"$2\\\"\\n\\\"$3}'");
             var cpuLines = manageProcess.Execute(machineMetricsProcessStartInfo).Split("\n");
-            machineMetrics.CPU.Cores = Convert.ToInt32(cpuLines[0].ToString());
-            machineMetrics.CPU.Load1min = Convert.ToSingle(cpuLines[1].ToString());
-            machineMetrics.CPU.Load5min = Convert.ToSingle(cpuLines[2].ToString());
-            machineMetrics.CPU.Load15min = Convert.ToSingle(cpuLines[3].ToString());
+            cPU.Cores = Convert.ToInt32(cpuLines[0].ToString());
+            cPU.Load1min = Convert.ToSingle(cpuLines[1].ToString());
+            cPU.Load5min = Convert.ToSingle(cpuLines[2].ToString());
+            cPU.Load15min = Convert.ToSingle(cpuLines[3].ToString());
             machineMetricsProcessStartInfo = manageProcess.BuildProcessStartInfo("/usr/bin/free -m|/usr/bin/head -2|/usr/bin/tail -n +2|/usr/bin/awk '{print $2\\\"\\n\\\"$3\\\"\\n\\\"$4\\\"\\n\\\"$6\\\"\\n\\\"$7}'");
             var memlines = manageProcess.Execute(machineMetricsProcessStartInfo).Split("\n");
-            machineMetrics.MemoryInMB.Total = Convert.ToInt32(memlines[0].ToString());
-            machineMetrics.MemoryInMB.Used = Convert.ToInt32(memlines[1].ToString());
-            machineMetrics.MemoryInMB.Free = Convert.ToInt32(memlines[2].ToString());
-            machineMetrics.MemoryInMB.Buffers = Convert.ToInt32(memlines[3].ToString());
-            machineMetrics.MemoryInMB.Available = Convert.ToInt32(memlines[4].ToString());
+            memoryInMB.Total = Convert.ToInt32(memlines[0].ToString());
+            memoryInMB.Used = Convert.ToInt32(memlines[1].ToString());
+            memoryInMB.Free = Convert.ToInt32(memlines[2].ToString());
+            memoryInMB.Buffers = Convert.ToInt32(memlines[3].ToString());
+            memoryInMB.Available = Convert.ToInt32(memlines[4].ToString());
             machineMetricsProcessStartInfo = manageProcess.BuildProcessStartInfo("/usr/bin/cat /proc/uptime|/usr/bin/awk '{print int($1)}'");
             var uptimeLines = manageProcess.Execute(machineMetricsProcessStartInfo).Split("\n");
-            machineMetrics.UptimeInSeconds.Uptime = Convert.ToInt32(uptimeLines[0].ToString());
+            uptimeInSeconds.Uptime = Convert.ToInt32(uptimeLines[0].ToString());
+            machineMetrics.CPU = cPU;
+            machineMetrics.MemoryInMB = memoryInMB;
+            machineMetrics.UptimeInSeconds = uptimeInSeconds;
             return machineMetrics;
         }
     }
