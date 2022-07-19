a5ccb455e469e2a80a0f02edbb0d50081ce8d934
Michael Lucas <mike@lucasnet.org>
2022-07-19 12:27:49 -0500

    Update Build.cs

--- app/build/Build.cs
+++ app/build/Build.cs
@@ -97,7 +97,7 @@ class Build : NukeBuild
                 .SetConfiguration(Configuration)
                 .EnableNoBuild()
                 .EnableNoRestore()
-                .SetPackageVersion(BuildVersion)
+                .SeteVersion(BuildVersion)
                 .SetOutputDirectory(ArtifactsDirectory / "nuget"));
         });
 
